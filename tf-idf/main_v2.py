d33ba8fc47f99280c715302724a591b492211f0b
wenlilong <zndxwll@gmail.com>
2021-02-04 19:19:23 +0800

    remove tf from query

--- tf-idf/main_v2.py
+++ tf-idf/main_v2.py
@@ -24,9 +24,10 @@ def tokenize(s_data):
 
     translator = str.maketrans('', '', string.punctuation)
     modified_string = s_data.translate(translator)
+    # modified_string = s_data
     # for chinese string
     modified_string = jieba.cut(modified_string)
-    tokenized_str = [item for item in modified_string]
+    tokenized_str = [item for item in modified_string if item is not " "]
     # for English
     #tokenized_str = nltk.word_tokenize(modified_string)
 
@@ -89,8 +90,19 @@ def calculate_tf(tokens):
 
     for token in tokens:
         tf_scores[token] = tokens.count(token)
+        tf_scores[token] = 1
 
     return tf_scores
+
+def calculate_tf_q(tokens):
+
+    tf_scores = {}
+
+    for token in tokens:
+        tf_scores[token] = 1
+
+    return tf_scores
+
 
 def calculate_tfidf(data, idf_score):
 
@@ -105,7 +117,7 @@ def calculate_tfidf(data, idf_score):
             tf = score
             idf = idf_score[token]
             tf_score[token] = tf * idf
-        tf_idf_scores[doc_num] = tf_score
+        tf_idf_scores[str(doc_num)] = tf_score
 
     return tf_idf_scores
 
@@ -115,7 +127,7 @@ def calculate_tfidf_queries(queries, idf_score):
     tf_scores = {}
 
     for key, value in queries.items():
-        tf_scores[key] = calculate_tf(value)
+        tf_scores[key] = calculate_tf_q(value)
 
     for key, tf_score in tf_scores.items():
         for token, score in tf_score.items():
@@ -125,7 +137,7 @@ def calculate_tfidf_queries(queries, idf_score):
             else:
                 idf = 0
             tf_score[token] = tf * idf
-        q_tf_idf_scores[key] = tf_score
+        q_tf_idf_scores[str(key)] = tf_score
 
     return q_tf_idf_scores
 
@@ -152,12 +164,10 @@ if __name__ == '__main__':
 
     queries = pre_process(read_data(args.query_path))
 
-    # print(pre_processed_data[0])
-    # print(queries[0])
-
     # inverted_index = generate_inverted_index(pre_processed_data)
     # idf_scores = calculate_idf(pre_processed_data)
     # scores = calculate_tfidf(pre_processed_data, idf_scores)
+
 
     if os.path.exists("inverted_index.json"):
         print("load inverted index")
@@ -181,6 +191,7 @@ if __name__ == '__main__':
         save_dict(scores, "scores")
 
     query_scores = calculate_tfidf_queries(queries, idf_scores)
+    # query_scores_sorted = sorted(query_scores.items(), key=operator.itemgetter(1), reverse=True)
 
     query_docs = {}
     for key, value in queries.items():
@@ -191,17 +202,26 @@ if __name__ == '__main__':
             if term in inverted_index.keys():
                 docs = inverted_index[term]
                 for doc in docs:
-                    doc = str(doc)
-                    doc_score = scores[doc][term]
-                    doc_length = math.sqrt(sum(x ** 2 for x in scores[doc].values()))
-                    query_score = query_scores[key][term]
-                    query_length = math.sqrt(sum(x ** 2 for x in query_scores[key].values()))
+                    # doc = str(doc)
+                    doc_score = scores[str(doc)][term]
+                    doc_length = math.sqrt(sum(x ** 2 for x in scores[str(doc)].values()))
+                    query_score = query_scores[str(key)][term]
+                    query_length = math.sqrt(sum(x ** 2 for x in query_scores[str(key)].values()))
                     cosine_sim = (doc_score * query_score) / (doc_length * query_length)
-
                     if doc in doc_sim.keys():
                         doc_sim[doc] += cosine_sim
                     else:
                         doc_sim[doc] = cosine_sim
+
+                    '''
+                    if str(doc) == '187':
+                        print(f'query doc num: {key}')
+                        print(f'term: {term}', end='  |')
+                        print(f'doc_num - doc_score: {doc} - {doc_score}', end=' | ')
+                        print(f'query_score: {query_score}', end='  |')
+                        print(f'cosine_sim: {cosine_sim}', end='  |')
+                        print()
+                    '''
 
         ranked = sorted(doc_sim.items(), key=operator.itemgetter(1), reverse=True)
 
